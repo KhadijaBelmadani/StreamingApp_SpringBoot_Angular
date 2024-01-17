842f8203d5b12bd7449fe260479a2b6a44496114
Khadija Belmadani <kbelmadani906@gmail.com>
2024-01-17 15:06:04 +0100

    Initial commit

---
+++ src/main/java/com/StreamingApp/example/StreamingApp/StreamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.StreamingApp.example.StreamingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingAppApplication.class, args);
+	}
+
+}
