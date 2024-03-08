177ff5ff0cdbaf78f9d95a1169c6639075611d1c
Khadija Belmadani <kbelmadani906@gmail.com>
2024-03-08 21:09:42 +0100

    commit

---
+++ frontend/streaming-app-ui/src/app/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userId:string=''
+
+  constructor(private httpClient:HttpClient) { }
+
+  subscribeToUser(userId:String):Observable<boolean>{
+  return this.httpClient.post<boolean>("http://localhost:8080/api/user/subscribe/"+userId,null);
+  }
+
+  registerUser() {
+     this.httpClient.get("http://localhost:8080/api/user/register",{responseType:'text'})
+       .subscribe(data=>{
+       this.userId=data;
+     })
+  }
+  getUserId():string{
+    return this.userId ;
+  }
+
+  unsubscribeToUser(userId: string):Observable<boolean> {
+    return this.httpClient.post<boolean>("http://localhost:8080/api/user/unSubscribe/"+userId,null);
+
+  }
+}
