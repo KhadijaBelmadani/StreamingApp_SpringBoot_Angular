a934cb49480dc49475de448edb78ae10f637e8b6
Khadija Belmadani <kbelmadani906@gmail.com>
2024-07-02 11:28:51 +0200

    commit

--- frontend/streaming-app-ui/src/app/featured/featured.component.ts
+++ frontend/streaming-app-ui/src/app/featured/featured.component.ts
@@ -10,13 +10,24 @@ import {UserService} from "../user.service";
 })
 export class FeaturedComponent implements OnInit{
   featuredVideos:Array<VideoDto>=[];
+  // titles = ['Creativity', 'Esport', 'Gaming', 'Lifestyle', 'Songs'];
+  // groupedFeaturedVideos: Array<VideoDto[]> = [];
+
   constructor(private videoService:VideoService,private userService:UserService) {
     this.userService.registerUser();
   }
   ngOnInit(): void {
+    // this.groupFeaturedVideos();
+
     this.videoService.getAllVideos().subscribe(response => {
       this.featuredVideos=response;
     });
   }
+
+  // groupFeaturedVideos() {
+  //   for (let i = 0; i < this.featuredVideos.length; i += 3) {
+  //     this.groupedFeaturedVideos.push(this.featuredVideos.slice(i, i + 3));
+  //   }
+  // }
 
 }
